c6980228b154a4b9621c49a9c1228ba2fdacb5b6
Marek Bodziony <marek_bodziony@o2.pl>
2017-01-26 15:47:30 +0100

    bug fixed - duplicate app when receiving NDEF message  (wile app is running on ShowCard screen)

--- app/src/main/java/mbodziony/businesscardsmanager/ShowCardActivity.java
+++ app/src/main/java/mbodziony/businesscardsmanager/ShowCardActivity.java
@@ -13,6 +13,7 @@ import android.nfc.NdefRecord;
 import android.nfc.NfcAdapter;
 import android.os.Bundle;
 import android.os.Environment;
+import android.os.Parcelable;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -70,6 +71,8 @@ public class ShowCardActivity extends AppCompatActivity {
     private static final int REQUEST_BLU = 1;
 
     private NfcAdapter nfcAdapter;
+    private PendingIntent nfcPendingIntent;
+    private IntentFilter[] ndefIntentFilters;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -108,8 +111,19 @@ public class ShowCardActivity extends AppCompatActivity {
         // set values of Card object taken from Intent (and hide empty fields)
         setMyCardValues();
 
-        // for NFC sharing
+        /**
+         * NfcAdapter and Intent filter for disabling duplicate app running when receiving Intent from NDEF message
+         */
         nfcAdapter = NfcAdapter.getDefaultAdapter(this);
+        nfcPendingIntent = PendingIntent.getActivity(this,0,new Intent(this,getClass()).setFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP),0);
+        IntentFilter ndefFilter = new IntentFilter(NfcAdapter.ACTION_NDEF_DISCOVERED);
+        try {
+            ndefFilter.addDataType("text/plain");                   // set payload type which interests us in NDEF message
+            ndefIntentFilters = new IntentFilter[]{ndefFilter};
+        }
+        catch (IntentFilter.MalformedMimeTypeException e) {
+            e.printStackTrace();
+        }
 
     }
 
@@ -267,8 +281,10 @@ public class ShowCardActivity extends AppCompatActivity {
     public void onResume(){
         super.onResume();
         // if NFC is enable on device set NDEF message ready for sharing via NFC
-        if (nfcAdapter != null && nfcAdapter.isEnabled())
-            nfcAdapter.setNdefPushMessage(putCardContentToNdefMessage(),this);
+        if (nfcAdapter != null && nfcAdapter.isEnabled()) {
+            nfcAdapter.setNdefPushMessage(putCardContentToNdefMessage(), this);
+        }
+        nfcAdapter.enableForegroundDispatch(this,nfcPendingIntent,ndefIntentFilters,null);
     }
 
     // share Card with other Android devices
@@ -427,5 +443,126 @@ public class ShowCardActivity extends AppCompatActivity {
                     .show();
         }
     }
+
+    /**
+     * Enable foreground dispatcher for handling Intent from NDEF message
+     */
+    /**
+     * Disable foreground dispatcher for handling Intent from NDEF message
+     */
+    @Override
+    public void onPause(){
+        super.onPause();
+        nfcAdapter.disableForegroundDispatch(this);
+    }
+    /**
+     * Handle Intent from NDEF message - forward to CardListActivity
+     */
+    @Override
+    public void onNewIntent (Intent intent){
+        Card c = getCardFromNdefMessage(intent);
+        startActivity(putCardInfoToIntent(c));
+    }
+
+    /**
+     * Method gets Card info from NDEF message
+     *
+     * @param intent The Intent from NDEF message
+     * @return card Card object
+     */
+    private Card getCardFromNdefMessage(Intent intent){
+        Card card = null;
+        NdefMessage[] msgs = null;
+        Parcelable[] rawMsg = intent.getParcelableArrayExtra(NfcAdapter.EXTRA_NDEF_MESSAGES);
+        if (rawMsg != null){
+            msgs = new NdefMessage[rawMsg.length];
+            for (int i = 0; i < rawMsg.length; i++){
+                msgs[i] = (NdefMessage)rawMsg[i];
+            }
+        }
+        // if there will be 3 NDEF records in NDEF message take payload from first two records (card details and logoImg)
+        if (msgs[0].getRecords().length == 3){
+            card = getCardFromJSON(msgs[0].getRecords()[0].getPayload());
+            card.setLogoImgPath(new String(msgs[0].getRecords()[1].getPayload()));
+            card.setId(0);
+        }
+        // if there will be 2 NDEF records in NDEF message take payload from first record (card details)
+        else if (msgs[0].getRecords().length == 2){
+            card = getCardFromJSON(msgs[0].getRecords()[0].getPayload());
+            card.setId(0);
+        }
+        // if there will less than 2 NDEF records in NDEF message set default values
+        else{
+            card = new Card("null","card_name","card_mobile","card_phone","card_fax","card_email","card_web","card_company",
+                    "card_address","card_job","card_facebook","card_tweeter","card_skype","card_other");
+            card.setId(0);
+        }
+
+        Log.d("CardNFC","Received NDEF message (" + msgs[0].getRecords().length + " records)");
+
+        return card;
+    }
+
+    /**
+     * Method gets Card information from JSON file
+     *
+     * @param payload_card_details Information in byte[] with Card details
+     * @return card Card object
+     */
+    private Card getCardFromJSON(byte[] payload_card_details){
+
+        Card card = null;
+        try{
+            JSONObject cardJSON = new JSONObject(new String(payload_card_details));
+            String logoPath = cardJSON.getString("logoPath");
+            String name = cardJSON.getString("name");
+            String mobile = cardJSON.getString("mobile");
+            String phone = cardJSON.getString("phone");
+            String fax = cardJSON.getString("fax");
+            String email = cardJSON.getString("email");
+            String web = cardJSON.getString("web");
+            String company = cardJSON.getString("company");
+            String address = cardJSON.getString("address");
+            String job = cardJSON.getString("job");
+            String facebook = cardJSON.getString("facebook");
+            String tweeter = cardJSON.getString("tweeter");
+            String skype = cardJSON.getString("skype");
+            String other = cardJSON.getString("other");
+
+            card = new Card(logoPath,name,mobile,phone,fax,email,web,company,address,job,facebook,tweeter,skype,other);
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+        Log.d("CardNFC","Card from JSON created");
+        return card;
+    }
+    /**
+     * Method puts Card details to Intent
+     *
+     * @param card Card object
+     * @return intent Intent object witch will be sent to CardsListActivity
+     */
+    // private method put MyCard data (fields) to Intent object
+    private Intent putCardInfoToIntent(Card card){
+        Intent i = new Intent(this,CardsListActivity.class).setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        i.putExtra("action","newNFC");
+        i.putExtra("logoPath","null");  // receiving Logo via NFC not supported in this app version
+        i.putExtra("name",card.getName());
+        i.putExtra("mobile",card.getMobile());
+        i.putExtra("phone",card.getPhone());
+        i.putExtra("fax",card.getFax());
+        i.putExtra("email",card.getEmail());
+        i.putExtra("web",card.getWeb());
+        i.putExtra("company",card.getCompany());
+        i.putExtra("address",card.getAddress());
+        i.putExtra("job",card.getJob());
+        i.putExtra("facebook",card.getFacebook());
+        i.putExtra("tweeter",card.getTweeter());
+        i.putExtra("skype",card.getSkype());
+        i.putExtra("other",card.getOther());
+        return i;
+    }
+
 
 }
